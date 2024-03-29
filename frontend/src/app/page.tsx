3430e1e6d05944a1e580c60a99e71d8e825c4483
Milan Polacek <milan.polacek.getsjob@gmail.com>
2024-03-29 10:33:31 +0100

    feat: initial FE commit

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-6xl font-bold">Hello Pokedex</h1>
+      <p className="text-2xl">This is a simple Pokedex app built with Next.js and Tailwind CSS</p>
+    </main>
+  );
+}
