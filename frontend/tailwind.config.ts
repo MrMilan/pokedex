9c742d10dc5b0b185b6ff008e339730112986c4b
Milan Polacek <milan.polacek.getsjob@gmail.com>
2024-04-02 19:50:28 +0200

    feat: [POK-3] add variables and styles

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,20 +1,32 @@
+import formsPlugin from "@tailwindcss/forms";
+
 import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/modules/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: "rgb(var(--primary) / <alpha-value>)",
+        secondary: "rgb(var(--secondary) / <alpha-value>)",
+        tertiary: "rgb(var(--tertiary) / <alpha-value>)",
+        negative: "rgb(var(--negative) / <alpha-value>)",
+        "bg-primary": "rgb(var(--bg-primary) / <alpha-value>)",
+        "bg-secondary": "rgb(var(--bg-secondary) / <alpha-value>)",
+        "bg-tertiary": "rgb(var(--bg-tertiary) / <alpha-value>)",
+        "fg-primary": "rgb(var(--fg-primary) / <alpha-value>)",
+        "fg-secondary": "rgb(var(--fg-secondary) / <alpha-value>)",
+        "fg-tertiary": "rgb(var(--fg-tertiary) / <alpha-value>)",
+        favorite: "rgb(var(--favorite) / <alpha-value>)",
+        "hp-bar": "rgb(var(--hp-bar) / <alpha-value>)",
+        "cp-bar": "rgb(var(--cp-bar) / <alpha-value>)",
       },
     },
   },
-  plugins: [],
+  plugins: [formsPlugin],
 };
 export default config;
